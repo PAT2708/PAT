d55c7720ab260e85fa9aab5b8913e0ae4e4c7d46
PAT2708 <phatltps26905@fpt.edu.vn>
2022-10-20 16:40:40 +0700

    f

---
+++ scrip.js
@@ -0,0 +1,6 @@
+const menuBar = document.querySelector(".menu-bar")
+menuBar.addEventListener("click",function(){
+    menuBar.classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")
+
+})
